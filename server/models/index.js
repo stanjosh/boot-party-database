88033c3c5dd8d625d2ade9d20c45f036f340238e
Joshua Stanford <stanjosh@gmail.com>
2023-12-06 16:26:43 -0600

    refactor: no product model necessary

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,6 @@
 const User = require('./User');
 const Customer = require('./Customer');
 const Event = require('./Event');
-const Product = require('./Product');
 
 
-module.exports = { User, Event, Product, Customer };
+module.exports = { User, Event, Customer };
