33be5c2fbde298b6b9ea3d6618d06f9f9b661b68
Joshua Stanford <stanjosh@gmail.com>
2023-12-11 13:35:41 -0600

    refactor: graphqlpath

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ const startApolloServer = async () => {
   db.once('open', () => {
     app.listen(parseInt(PORT), () => {
       
-      console.log('Server ready at http://localhost:${PORT}${server.graphqlPath}');
+      console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
   };
