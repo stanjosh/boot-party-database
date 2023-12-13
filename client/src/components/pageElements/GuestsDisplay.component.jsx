f21efcc80fe0f763856f8d87739c0f66ce1d32e8
Joshua Stanford <stanjosh@gmail.com>
2023-12-13 16:07:09 -0600

    Refactor GuestsDisplay component to include event contact information

--- client/src/components/pageElements/GuestsDisplay.component.jsx
+++ client/src/components/pageElements/GuestsDisplay.component.jsx
@@ -7,10 +7,26 @@ const GuestsDisplay = ( { eventData } ) => {
 
     return (
         <>
-            
+        <Card><h3 style={{ fontSize: '5cqb' }}>Guests</h3></Card>
+            <Card key={eventData?.eventContact?._id} style={{marginTop: "5px"}}>
+                <Card.Body>
+                    
+                    <Card.Text>
+                        <strong>Name:</strong> {eventData?.eventContact?.name}<br />
+                        <strong>Email:</strong> {eventData?.eventContact?.email}<br />
+                        <strong>Phone:</strong> {eventData?.eventContact?.phone ? eventData?.eventContact?.phone : 'not provided'}<br />
+                        <strong>Boot:</strong> {eventData?.eventContact?.bootName}<br />
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
         {eventData?.eventSignups.length > 0 ? (
             <>
-            <Card><h3 style={{ fontSize: '5cqb' }}>Guests</h3></Card>
+
+
+
+
+
           {eventData?.eventSignups.map((signup) => (
             <Card key={signup._id} style={{marginTop: "5px"}}>
                 <Card.Body>
