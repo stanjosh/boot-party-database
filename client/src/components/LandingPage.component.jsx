e940e0e04424ae1783b5c1c7a83b1356ae0a94ba
Joshua Stanford <stanjosh@gmail.com>
2023-12-22 22:13:44 -0600

    style: polaroid drop

--- client/src/components/LandingPage.component.jsx
+++ client/src/components/LandingPage.component.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 
 const LandingPage = () => {
+    const [scrollY, setScrollY] = useState(0);
+
+
+    useEffect(() => {
+      window.addEventListener("scroll", () => {
+        setScrollY(window.scrollY);
+      });
+      console.log(scrollY)
+    }, [scrollY])
+
     return (
       <>
         <div className="landingPageBox" style={{minWidth: "100%", textAlign: "center", alignContent: "center"}}>
@@ -17,11 +28,43 @@ const LandingPage = () => {
             minWidth: "100%", 
             justifyContent: "center", 
             alignContent: "Center", 
-            alignItems: "center"
+            alignItems: "center",
+            overflowY: "hidden"
           }}>
+            <div style={{position: "relative", width: "100cqw", height: "0"}}>
+              <div style={{
+                backgroundImage: "url(https://alvies-test.imgix.net/Image_20220613_0001.jpg?ar=1.04%3A1&auto=compress%2Cformat&dm=1656514443&fit=crop&ixlib=php-3.1.0&w=400)",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                position: "absolute", display: "block", width: "32cqh", height: "32cqh", top: `-18cqh`, right: "0%", backgroundColor: "#123455", zIndex: "4", transform: `translateY(${.45 * scrollY}px) rotate3d(2, 2, -1, ${scrollY * 0.04}deg)`} }>
+                <img src="https://alvies-test.imgix.net/polaroid/polaroid-frame.png?auto=compress%2Cformat&dm=1654211509&fit=clip&ixlib=php-3.1.0&w=400" style={{position: "relative", top: "-5px", left: "-5px", width: "34cqh", height: "34cqh"}}></img>
+              </div>
+              <div style={{
+                backgroundImage: "url(https://alvies-test.imgix.net/IMG-1037.jpg?ar=1.04%3A1&auto=compress%2Cformat&dm=1656515997&fit=crop&ixlib=php-3.1.0&w=400)",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                position: "absolute", display: "block", width: "32cqh", height: "32cqh", top: `-48cqh`, right: "0%", backgroundColor: "#123455", zIndex: "4", transform: `translateY(${.30 * scrollY}px) rotate3d(2,-1,0, ${scrollY * -0.03}deg)`} }>
+                <img src="https://alvies-test.imgix.net/polaroid/polaroid-frame.png?auto=compress%2Cformat&dm=1654211509&fit=clip&ixlib=php-3.1.0&w=400" style={{position: "relative", top: "-5px", left: "-5px", width: "34cqh", height: "34cqh"}}></img>
+              </div>
+              <div style={{
+                backgroundImage: "url(https://alvies-test.imgix.net/gsmith_20220611_0184.jpg?ar=1.04%3A1&auto=compress%2Cformat&dm=1656514656&fit=crop&ixlib=php-3.1.0&w=300)",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                position: "absolute", display: "block", width: "32cqh", height: "32cqh", top: `-48cqh`, left: "0%", backgroundColor: "#123455", zIndex: "4", transform: `translateY(${.30 * scrollY}px) rotate3d(2, -1, -2, ${scrollY * 0.05}deg)`} }>
+                <img src="https://alvies-test.imgix.net/polaroid/polaroid-frame.png?auto=compress%2Cformat&dm=1654211509&fit=clip&ixlib=php-3.1.0&w=400" style={{position: "relative", top: "-5px", left: "-5px", width: "34cqh", height: "34cqh"}}></img>
+              </div>
+              <div style={{
+                backgroundImage: "url(https://alvies-test.imgix.net/gsmith_20220611_0264.jpg?ar=1.04%3A1&auto=compress%2Cformat&dm=1656515268&fit=crop&ixlib=php-3.1.0&w=300)",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                position: "absolute", display: "block", width: "32cqh", height: "32cqh", top: `0cqh`, left: "0%", backgroundColor: "#123455", zIndex: "4", transform: `translateY(${.30 * scrollY}px) rotate3d(-3, -4, 3, ${scrollY * 0.03}deg)`} }>
+                <img src="https://alvies-test.imgix.net/polaroid/polaroid-frame.png?auto=compress%2Cformat&dm=1654211509&fit=clip&ixlib=php-3.1.0&w=400" style={{position: "relative", top: "-5px", left: "-5px", width: "34cqh", height: "34cqh"}}></img>
+              </div>
+            </div>
           <div className="landingPageTextBlock" style={{maxWidth:"600px"}}>
             <h2 className="landingPageTitle">KICK UP YOUR FEET</h2>
             <h3 className="landingPageTitle">WE&apos;LL COME TO YOU</h3>
+
             <p style={{fontFamily: '"work-sans",sans-serif', fontWeight: "600",  fontSize: "1.5cqh" }}>
               We want to provide the best customer experience in all of footwear. Because if you can’t have fun trying on boots, you’re doing it wrong. We will literally drive a van-full of boots to you for a personal boot-fitting, wherever you are. Your couch? Cool. Work, no problemo. Got a favorite park bench? Drop us a pin. We want you to feel comfortable trying on our boots, and pulling them off. We’ll bring a few sizes and styles of your choice, for you (and whoever else may be joining) to find the perfect pair, and answer all your boot-wearin’ questions with trusted expertise.</p>
             <Button href="/book" className="landingPageButton">LET&apos;S PARTY</Button>
@@ -48,6 +91,7 @@ const LandingPage = () => {
             <p>- Stephanie Martin</p>
           </div>
 				
+
  
           <div className="pblock" style={{textAlign: "right", alignSelf: "end", display: "inline-block"}}>
               <h4>CHOOSE YOUR BOOT PARTY</h4>
@@ -88,7 +132,7 @@ const LandingPage = () => {
               
         <div style={{width: "100%", backgroundColor: "aliceblue", paddingTop: "15px", paddingBottom: "15px", textAlign: "center"}}>
           <Button href="/book" className="landingPageButton">SIGN ME UP</Button>
-          </div>
+        </div>
   
 
         
