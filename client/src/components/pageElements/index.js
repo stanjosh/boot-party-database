01c2ff338362b64691fe67c35707da8543ee48e2
Joshua Stanford <stanjosh@gmail.com>
2023-12-22 22:17:48 -0600

    refactor: Polaroiddrop to component

--- client/src/components/pageElements/index.js
+++ client/src/components/pageElements/index.js
@@ -3,6 +3,6 @@ import Footer from './Footer.component';
 import EventDisplay from './EventDisplay.component';
 import GuestsDisplay from './GuestsDisplay.component';
 import BootSelect from '../forms/bootSelect/BootSelect.component';
+import PolaroidDrop from './PolaroidDrop.component';
 
-
-export { Header, Footer, EventDisplay, GuestsDisplay, BootSelect };
+export { Header, Footer, EventDisplay, GuestsDisplay, BootSelect, PolaroidDrop};
