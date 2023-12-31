75cde45faa863635b99c90babef444378a2ca498
Joshua Stanford <stanjosh@gmail.com>
2023-12-31 12:45:39 -0600

    style: header dropdown

--- client/src/components/pageElements/Header.component.jsx
+++ client/src/components/pageElements/Header.component.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
-import { Button, Navbar, Nav, Container, Modal, Alert, Offcanvas } from 'react-bootstrap';
+import { Button, Navbar, Nav, NavDropdown, Container, Modal, Alert, Offcanvas } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/client';
 //import { QUERY_ME } from '../../util/queries';
 import { LOGIN_USER } from '../../util/mutations';
-
 import Form from 'react-bootstrap/Form';
 import Auth from '../../util/Auth';
 
@@ -12,6 +11,8 @@ const Header = () => {
   const [userData, setUserData] = useState({});
   // set modal display state
   const [showModal, setShowModal] = useState(false);
+  const [showOffCanvas, setShowOffCanvas] = useState(false);
+
 
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
@@ -59,13 +60,32 @@ const Header = () => {
       <>
       
       {['sm'].map((expand) => (
-        <Navbar key={expand} expand={expand} style={{zIndex: "100"}} className="bg-body-tertiary mb-3">
+        <Navbar key={expand} expand={expand} style={{zIndex: "100", fontFamily: "unpretentious"}} className="bg-body-tertiary mb-3">
           <Container fluid>
             <Navbar.Brand href="/"><img src='/boot-party-blue.png' height={'70cqb'} /></Navbar.Brand>
-           
+            
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              
+          <Nav className="me-auto">
+            <Button onClick={() => setShowModal(true)}>LOGIN</Button>
+            <Nav.Link href="/book" style={{width: "100%", textAlign: "right"}}>Book a Party</Nav.Link>
+            <Nav.Link href="/manage" style={{width: "100%", textAlign: "right"}}>Manage a Party</Nav.Link>
+            
+            <Nav.Link href="#deets" style={{width: "100%", textAlign: "right"}}>FAQ / CONTACT</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes" style={{width: "100%", textAlign: "right"}}>
+              SHOP ALVIES
+            </Nav.Link>
+
+              
+            
+          </Nav>
+
+        </Navbar.Collapse>
           </Container>
         </Navbar>
       ))}
+
 
       
       <Modal
